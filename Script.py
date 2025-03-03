3a6cd37444192ea9fbf1ceea9f4cb242c1314797
yogirajb <108505047+yogirajb@users.noreply.github.com>
2025-03-03 14:59:23 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -244,7 +244,7 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
+    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : @MzMoviiez {file_name}
 
 <b>⚙️ sɪᴢᴇ : {file_size}</b>""" 
 
